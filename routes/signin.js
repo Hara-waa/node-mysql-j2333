2c2d5bd5cb03a959951884c232bff71ce228f43a
yukina-hara <tsuwabuki0922@gmail.com>
2026-08-04 02:45:17 +0000

    TODOタスクとユーザを紐付けよう

--- routes/signin.js
+++ routes/signin.js
@@ -3,30 +3,18 @@ const router = express.Router();
 const passport = require("passport");
 
 router.get('/', function (req, res, next) {
-  const isAuth = Boolean(req.session.userid || (req.user && req.user.id));
+  const isAuth = req.isAuthenticated();
   res.render("signin", {
     title: "Sign in",
     isAuth: isAuth,
   });
 });
 
-router.post('/', function (req, res, next) {
-  passport.authenticate('local', function (err, user) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.redirect('/signin');
-    }
-
-    req.logIn(user, function (loginErr) {
-      if (loginErr) {
-        return next(loginErr);
-      }
-      req.session.userid = user.id;
-      return res.redirect('/');
-    });
-  })(req, res, next);
-});
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/signin',
+    failureFlash: true,
+  }
+));
 
 module.exports = router;
